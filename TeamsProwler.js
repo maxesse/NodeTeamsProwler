fb9b91e8c2a87fc713e759a967fe2b55a3fc2bd9
Max Sanna <max@maxsanna.com>
2021-07-28 14:48:28 +0200

    Added option for win32, win64 or mac

--- TeamsProwler.js
+++ TeamsProwler.js
@@ -7,7 +7,8 @@ import os from "os"
 
 const yarg = yargs(hideBin(process.argv))
 
-const options = yarg.usage("Usage: -mv <Major Teams version> -b <Starting Build> -c <Builds ahead to lookup>")
+const options = yarg.usage("Usage: -o <win32/win64/macos> -m <Major Teams version> -b <Starting Build> -c <Builds ahead to lookup>")
+    .option("o", { alias: "os", describe: "Use 'win32', 'win64' or 'mac' to choose which build to download", type: "string", demandOption: true})
     .option("m", { alias: "majorversion", describe: "Major Teams Version (i.e. 1.4.00)", type: "string", demandOption: true})
     .option("b", { alias: "build", describe: "Start searching from (i.e. 10000)", type: "string", demandOption: true})
     .option("c", { alias: "count", describe: "Builds to search for (i.e. 100)", type: "string", demandOption: true})
@@ -19,7 +20,16 @@ const count = options.count
 var versionsToCheck = new Array()
 var existingVersions = new Array()
 for (var version = Number(build); version < Number(build) + Number(count); version++) {
-    versionsToCheck.push(`https://statics.teams.microsoft.com/production-osx/${majorVersion}.${version}/Teams_osx.pkg`)
+    if (options.os === "win32") {
+        versionsToCheck.push(`https://statics.teams.microsoft.com/production-windows/${majorVersion}.${version}/Teams_windows.exe`)
+    } else if (options.os === "win64") {
+        versionsToCheck.push(`https://statics.teams.microsoft.com/production-windows-x64/${majorVersion}.${version}/Teams_windows_x64.exe`)
+    } else if (options.os === "mac") {
+        versionsToCheck.push(`https://statics.teams.microsoft.com/production-osx/${majorVersion}.${version}/Teams_osx.pkg`)
+    } else {
+        console.log("Please enter a valid options for the OS ('win32', 'win64' or 'mac')")
+        process.exit()
+    }
 }
 
 var promise1 = Promise.all(versionsToCheck.map(async url => {
